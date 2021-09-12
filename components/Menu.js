c346908862d1ae142e876d97675b6ca38b3e4464
wefq <wefqshell@gmail.com>
2021-09-12 19:19:57 +0300

    'third-commit'

---
+++ components/Menu.js
@@ -0,0 +1,19 @@
+const showMenu = () => {
+  document.querySelector('.drop-menu_content').classList.toggle('drop-menu_show')
+
+}
+
+window.onClick = function (event) {
+
+  console.log(event)
+  if (!event.target.matches('.drop-menu_btn')) {
+
+    const menuContent = document.querySelector('.drop-menu_content')
+
+    if (menuContent.classList.contains('drop-menu_show')) menuContent.classList.remove('drop-menu_show')
+  }
+}
+
+const BUTTON = document.querySelector('.drop-menu_btn')
+
+BUTTON.addEventListener('click', showMenu)
